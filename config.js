b1cc03d41c247ab0d779834b7e7db7aeb7d7dfa7
mo <mam283887@gmail.com>
2026-02-23 18:47:07 +0300

    Update user credentials and officer ranks in config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 ﻿// ==========================================// الحسابات الأساسية التي لا تُحذف أبداً (تقدر تضيف القيادات هنا)
 const ConfigUsers = [
     { user: "admin", pass: "123", name: "dev", rank: "Minister of Interior" },
-    { user: "L.S.P.D Chief", pass: "123", name: "أحمد الفهد", rank: "L.S.P.D Chief" },
+    { user: "2zcp", pass: "123", name: "أحمد الفهد", rank: "L.S.P.D Chief" },
     { user: "fzzzl2", pass: "123", name: "دانيل سميث", rank: "Lieutenant" },
     { user: "Saeed Faiz", pass: "123", name: "سعيد فايز", rank: "Minister of Interior" },
 ];
@@ -37,6 +37,7 @@ const EnlistedRanks = [
     "Solo Cadet", 
     "Cadet"
 ];
+
 
 
 
